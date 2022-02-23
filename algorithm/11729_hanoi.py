81570920dd26fa49200867ac6057ca8e3f0387f2
hohyun <ioio6382@gmail.com>
2022-02-24 07:11:44 +0900

    First Commit

--- algorithm/11729_hanoi.py
+++ algorithm/11729_hanoi.py
@@ -1,12 +1,14 @@
-N, M = map(int, input().split())
-list_num = list(map(int, input().split()))
-result = 0
+import sys
+sys.setrecursionlimit(10**6)
 
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            sum_ijk = list_num[i] + list_num[j] + list_num[k]
-            if sum_ijk <= M and list_num[i] + sum_ijk > result:
-                result = sum_ijk
+def hanoi(n, start, temp, end):
+    if n==1:
+        print(start, end)
+    else:
+        hanoi(n-1, start, end, temp)
+        print(start, end)
+        hanoi(n-1, temp, start, end)
 
-print(result)
+n = int(input())
+print(2**n - 1)
+hanoi(n, 1, 2, 3)
