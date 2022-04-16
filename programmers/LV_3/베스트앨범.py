8a95acfbcc9b55972859648ce9e9d0edb1cc8980
Hohyun Park <ioio6382@gmail.com>
2022-04-16 21:47:21 +0900

    2022-04-16

--- programmers/LV_3/베스트앨범.py
+++ programmers/LV_3/베스트앨범.py
@@ -1,22 +1,36 @@
 def solution(genres, plays):
-    answer = []
-
-    dic1 = {}
-    dic2 = {}
-
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in dic1:
-            dic1[g] = [(i, p)]
+    # 자료구조: dictionary
+    # key: genres / value: [총합, [[plays, index], [plays, index], ... ]]
+    dic = {}
+    
+    # genres와 plays를 쌍으로 dictionary에 넣어주기
+        # genre가 없을 경우 초기 세팅
+        # 있을 경우 총합은 더해주고, 배열에는 append 해줌
+    i = 0
+    for genre, play in zip(genres, plays):
+        if genre in dic:
+            dic[genre][0] += play
+            dic[genre][1].append([play, i])
         else:
-            dic1[g].append((i, p))
+            dic[genre] = [play, [[play, i]]]
+        i += 1
+        
+    # dictinary 자체를 총합에 따라 정렬해줌
+    # dictinary 각 원소마다 배열의 값을 정렬해줌
+    d = sorted(dic.items(), key=lambda x: x[1][0], reverse=True)
+    result = []
+    target_length = len(genres) if len(genres)%2 == 0 else len(genres)-1
+    for i in range(len(d)):
+        d[i][1][1].sort(reverse=True)
+        for _, idx in d[i][1][1]:
+            result.append(idx)
+            if len(result) == target_length:
+                break
 
-        if g not in dic2:
-            dic2[g] = p
-        else:
-            dic2[g] += p
-
-    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
-        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
-            answer.append(i)
-
-    return answer
+    return result
+    
+    
+    # 장르수//2 만큼 dictionary의 values를 하나씩 결과값에 넣어줌
+    
+    
+print(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]	))
---
+++ programmers/LV_3/여행경로.py
@@ -0,0 +1,59 @@
+def solution(tickets):
+    dic = {}
+    for start, end in tickets:
+        if start in dic:
+            dic[start].append(end)
+        else:
+            dic[start] = [end]
+    for key in dic.keys():
+        dic[key].sort(reverse=True)
+    
+    answer = []
+    stack = ["ICN"]
+    while stack:
+        airport = stack[-1]
+        if airport in dic and dic[airport]:
+            stack.append(dic[airport].pop())
+        else:
+            answer.append(stack.pop())
+    return answer[::-1]
+
+print(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "ICN"]]))
+
+
+# from collections import deque
+
+# def solution(tickets):
+#     # dictionary
+#     # key: 출발지 / value: [[도착지],0]
+#     dic = {}
+    
+#     # tickets를 dictionary에 넣어줌
+#         # 있을 경우 value에 도착지만 append
+#         # 없을 경우 초기 세팅
+#     for start, end in tickets:
+#         if start in dic:
+#             dic[start][0].append(end)
+#         else:
+#             dic[start] = [[end], 0]
+    
+#     for key, value in dic.items():
+#         if len(dic[key][0]) > 1:
+#             dic[key][0].sort()
+    
+#     result = []
+#     # BFS를 활용하기
+#     queue = deque()
+#     queue.append("ICN")
+#     while queue:
+#         start = queue.popleft()
+#         result.append(start)
+#         if start not in dic:
+#             break
+#         ends, idx = dic[start]
+#         if idx == len(ends):
+#             break
+#         queue.append(ends[idx])
+#         dic[start][1] += 1
+        
+#     return result
