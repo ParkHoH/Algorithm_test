d2b6433a8c44398ae7b1b4d0e34b4620519ce79d
Hohyun Park <ioio6382@gmail.com>
2022-04-18 00:27:37 +0900

    2022-04-18

---
+++ programmers/LV_3/단속카메라.py
@@ -0,0 +1,14 @@
+def solution(routes):
+    routes.sort()
+    result = 1
+    start, end = routes[0]
+    for i in range(1, len(routes)):
+        if routes[i][0] in range(start, end+1) or routes[i][1] in range(start, end+1):
+            start = min(start, routes[i][0])
+            end = min(end, routes[i][1])
+        else:
+            start, end = routes[i]
+            result += 1
+    return result
+
+print(solution([[-20, -15], [-14, -5], [-18, -13], [-5, -3]]))
