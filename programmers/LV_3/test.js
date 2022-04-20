105077625be929976a877fb9c4d5d9e9f375e019
Hohyun Park <ioio6382@gmail.com>
2022-04-21 00:38:13 +0900

    2022-04-21

---
+++ programmers/LV_3/test.js
@@ -0,0 +1,3 @@
+document.getElementById('버튼').addEventListener('click', 
+    setTimeout(()=>{console.log(this.innerHTML)},5000)
+  );
--- programmers/LV_3/표 편집.py
+++ programmers/LV_3/표 편집.py
@@ -1,4 +1,47 @@
 def solution(n, k, cmd):
+    linked = {i: [i-1, i+1] for i in range(n)}
+    linked[0], linked[n-1] = [None, 1], [n-2, None]
+    eliminated_list = []
+    result = ["O"] * n
+    for command in cmd:
+        if command[0] == "D" or command[0] == "U": # 커서 이동
+            direction, num = command.split()
+            if direction == "D":
+                for _ in range(int(num)):
+                    k = linked[k][1]
+            else:
+                for _ in range(int(num)):
+                    k = linked[k][0]
+            
+        elif command == "C": # 삭제
+            result[k] = "X"
+            prev, after = linked[k]
+            eliminated_list.append([k, prev, after])
+            k = prev if after == None else after
+            if prev == None:
+                linked[after][0] = prev
+            elif after == None:
+                linked[prev][1] = after
+            else:
+                linked[prev][1] = after
+                linked[after][0] = prev
+        
+        else: # 되돌리기
+            idx, prev, after = eliminated_list.pop()
+            result[idx] = "O"
+            if prev and after:
+                linked[prev][1] = idx
+                linked[after][0] = idx
+            elif prev:
+                linked[prev][1] = idx
+            elif after:
+                linked[after][0] = idx
+                
+    return ''.join(result)
+
+
+# my solution: 잘못된 부분
+def solution(n, k, cmd):
     cmd = [command.split() for command in cmd]
     L = []
     L.append([0, 0, 1, "O"])
@@ -57,56 +100,57 @@ def solution(n, k, cmd):
             
     return ''.join([result[3] for result in L])
 
-# # 시간 초과 
-# def solution(n, k, cmd):
-#     cmd = [command.split() for command in cmd]
-#     L = []
-#     for i in range(n):
-#         L.append([i, "O"])
-#     delete = []
+
+# 시간 초과 
+def solution(n, k, cmd):
+    cmd = [command.split() for command in cmd]
+    L = []
+    for i in range(n):
+        L.append([i, "O"])
+    delete = []
         
-#     for command in cmd:
-#         if command[0] == "D":
-#             cnt = 0
-#             while cnt != int(command[1]):
-#                 if k == n-1:
-#                     break
-#                 k += 1
-#                 if L[k][1] == "O":
-#                     cnt += 1
+    for command in cmd:
+        if command[0] == "D":
+            cnt = 0
+            while cnt != int(command[1]):
+                if k == n-1:
+                    break
+                k += 1
+                if L[k][1] == "O":
+                    cnt += 1
                 
-#         elif command[0] == "U":
-#             cnt = 0
-#             while cnt != int(command[1]):
-#                 if k == 0:
-#                     break
-#                 k -= 1
-#                 if L[k][1] == "O":
-#                     cnt += 1
+        elif command[0] == "U":
+            cnt = 0
+            while cnt != int(command[1]):
+                if k == 0:
+                    break
+                k -= 1
+                if L[k][1] == "O":
+                    cnt += 1
                     
-#         elif command[0] == "C":
-#             L[k][1] = "X"
-#             ori_k = k
-#             delete.append(k)
-#             cnt = 0
-#             while cnt != 1:
-#                 if k == n-1:
-#                     k = ori_k
-#                     while cnt != 1:
-#                         if k == 0:
-#                             break
-#                         k -= 1
-#                         if L[k][1] == "O":
-#                             cnt += 1
-#                     break
-#                 k += 1
-#                 if L[k][1] == "O":
-#                     cnt += 1
+        elif command[0] == "C":
+            L[k][1] = "X"
+            ori_k = k
+            delete.append(k)
+            cnt = 0
+            while cnt != 1:
+                if k == n-1:
+                    k = ori_k
+                    while cnt != 1:
+                        if k == 0:
+                            break
+                        k -= 1
+                        if L[k][1] == "O":
+                            cnt += 1
+                    break
+                k += 1
+                if L[k][1] == "O":
+                    cnt += 1
         
-#         elif command[0] == "Z":
-#             idx = delete.pop()
-#             L[idx][1] = "O"
+        elif command[0] == "Z":
+            idx = delete.pop()
+            L[idx][1] = "O"
             
-#     return ''.join([result[1] for result in L])
+    return ''.join([result[1] for result in L])
 
 print(solution(	8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"]))
