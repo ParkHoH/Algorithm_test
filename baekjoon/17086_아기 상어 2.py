d91005b043b92a5d9dbbae81b52aa61402d3ca55
Hohyun Park <ioio6382@gmail.com>
2022-05-14 11:33:35 +0900

    2022-05-14

---
+++ baekjoon/17086_아기 상어 2.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+direction = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y, 1))
+    visited = [[False] * M for _ in range(N)]
+    visited[x][y] = True
+    while q:
+        x, y, distance = q.popleft()
+        for i in range(9):
+            dx, dy = direction[i]
+            nx, ny = x + dx, y + dy
+
+            if nx >= N or ny >= M or nx < 0 or ny < 0:
+                continue
+
+            if graph[nx][ny] == 1: # 아기 상어인 경우
+                return distance
+
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny, distance+1))
+            
+max_distance = 0
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            max_distance = max(max_distance, bfs(i, j))
+
+print(max_distance)
