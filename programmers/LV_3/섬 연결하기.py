e323b01dba5e506d2a5f4b0d9d32f435b1591d66
Hohyun Park <ioio6382@gmail.com>
2022-06-29 17:46:41 +0900

    2022-06-29

--- programmers/LV_3/섬 연결하기.py
+++ programmers/LV_3/섬 연결하기.py
@@ -13,3 +13,22 @@ def solution(n, costs):
                 costs[i] = [-1, -1, -1]
                 break
     return ans
+
+
+# 복습
+def solution(n, costs):
+    result = 0
+    costs.sort(key = lambda x: x[2])
+    connect = set([costs[0][0]])
+    
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect:
+                continue
+
+            if cost[0] in connect or cost[1] in connect:
+                result += cost[2]
+                connect.update([cost[0], cost[1]])
+                break
+                
+    return result
