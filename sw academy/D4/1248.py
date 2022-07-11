9769300e2a5631ec85fed05a2b0f73e01ea23028
Hohyun Park <ioio6382@gmail.com>
2022-07-11 19:05:49 +0900

    2022-07-11

--- sw academy/D4/1248.py
+++ sw academy/D4/1248.py
@@ -6,7 +6,7 @@ def find_parent(node, graph):
     while stack:
         node = stack.pop()
         if node in graph:
-            stack.append(graph[node])
+            stack.append(graph[node])                                       
             L.append(graph[node])
     
     return L
@@ -41,40 +41,3 @@ for t in range(1, TC+1):
             stack.append(i)
 
     print(f'#{t} {result[0]} {result[1]}')
-
-
-# from collections import defaultdict
-
-# TC = int(input())
-# for t in range(1, TC+1):
-#     V, E, node_1, node_2 = map(int, input().split())
-#     graph_child = defaultdict(list)
-#     L = list(map(int, input().split()))
-#     L_node_1 = [node_1]
-#     L_node_2 = [node_2]
-#     for i in range(E-1, -1, -1):
-#         graph_child[L[2*i]].append(L[2*i + 1])
-#         if L[2*i + 1] in L_node_1:
-#             L_node_1.append(L[2*i])
-
-#         if L[2*i + 1] in L_node_2:
-#             L_node_2.append(L[2*i])
-
-#     result = [0, 0]
-#     stop = False
-#     for i in L_node_1:
-#         for j in L_node_2:
-#             if j == i:
-#                 stop = True
-#                 result[0] = j
-#                 break
-#         if stop: break
-
-#     stack = [result[0]]
-#     while stack:
-#         node = stack.pop()
-#         result[1] += 1
-#         for i in graph_child[node]:
-#             stack.append(i)
-
-#     print(f'#{t} {result[0]} {result[1]}')
