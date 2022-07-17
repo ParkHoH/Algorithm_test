34dbc23db6212f73f44585c4cba9688ce885e7dc
Hohyun Park <ioio6382@gmail.com>
2022-07-17 22:50:24 +0900

    2022-07-17

--- programmers/LV_3/풍선 터트리기.py
+++ programmers/LV_3/풍선 터트리기.py
@@ -1,13 +1,13 @@
 def solution(a):
     result = [0] * len(a)
-    min_value = max_value = float("inf")
+    left_min = right_min = float('inf')
     for i in range(len(a)):
-        if a[i] < min_value:
-            min_value = a[i]
+        if a[i] < left_min:
+            left_min = a[i]
             result[i] = 1
-            
-        if a[-1-i] < max_value:
-            max_value = a[-1-i]
+        
+        if a[-1-i] < right_min:
+            right_min = a[-1-i]
             result[-1-i] = 1
-            
+        
     return sum(result)
