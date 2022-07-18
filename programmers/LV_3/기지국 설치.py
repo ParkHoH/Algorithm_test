046c252d21d56c8c3a3023dff341e329985af9f2
Hohyun Park <ioio6382@gmail.com>
2022-07-18 20:58:16 +0900

    2022-07-18

--- programmers/LV_3/기지국 설치.py
+++ programmers/LV_3/기지국 설치.py
@@ -1,14 +1,41 @@
-import math
-
 def solution(n, stations, w):
-    num = 2 * w + 1
-    location = 1
     result = 0
-    for station in stations:
-        result += max(math.ceil((station - location - w) / num), 0)
-        location = station + w + 1
-        
-    if n >= location:
-        result += math.ceil((n - location + 1) / num)
+    num = w * 2 + 1
+    front = 0
+    end = stations[0] - w
     
+    for i in range(len(stations)):
+        result += (end - front) // num + 1
+        if (end - front) % num == 0:
+            result -= 1
+        
+        if (i != 0 and front >= end) or i == len(stations) - 1:
+            continue
+
+        front = stations[i] + w
+        end = stations[i+1] - w
+    
+    if n > stations[-1] + w:
+        result += (n - (stations[-1] + w)) // num + 1
+        if (n - (stations[-1] + w)) % num == 0:
+            result -= 1
+        
     return result
+
+print(solution(	16, [9], 2))
+
+
+# import math
+
+# def solution(n, stations, w):
+#     num = 2 * w + 1
+#     location = 1
+#     result = 0
+#     for station in stations:
+#         result += max(math.ceil((station - location - w) / num), 0)
+#         location = station + w + 1
+        
+#     if n >= location:
+#         result += math.ceil((n - location + 1) / num)
+    
+#     return result
