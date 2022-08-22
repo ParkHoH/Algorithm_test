3c998ed20dc86fca74eea74baae1e9d82c7e6f94
Hohyun Park <ioio6382@gmail.com>
2022-08-22 17:59:42 +0900

    2022-08-22

---
+++ baekjoon/11053_가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = list(map(int, input().split()))
+dp = [-1] * (N)
+
+def dfs(idx):
+    if dp[idx] == -1:
+        dp[idx] = 1
+
+        for i in range(idx+1, N):
+            if A[i] > A[idx]:
+                dp[idx] = max(dp[idx], dfs(i)+1)
+
+    return dp[idx]
+
+for i in range(N):
+    dfs(i)
+    
+print(max(dp))
--- baekjoon/11398_연속합 2.py
+++ baekjoon/11398_연속합 2.py
@@ -2,14 +2,11 @@ n = int(input())
 L = list(map(int, input().split()))
 dp = [[0, 0] for _ in range(n)]
 
-dp[0][0] = L[0]
+dp[0][0] = result = L[0]
 
 for i in range(1, n):
     dp[i][0] = max(L[i], dp[i-1][0] + L[i])
     dp[i][1] = max(dp[i-1][0], dp[i-1][1] + L[i])
-
-result = L[0]
-for i in range(1, n):
     result = max(result, max(dp[i]))
 
 print(result)
