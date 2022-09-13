07be5da914dfc2f869baefe79fa4c9639893e9a9
Hohyun Park <ioio6382@gmail.com>
2022-09-13 22:11:54 +0900

    2022-09-13

--- programmers/LV_3/사라지는 발판.py
+++ programmers/LV_3/사라지는 발판.py
@@ -1,58 +1,96 @@
-dir = ((-1,0),(0,1),(1,0),(0,-1))
+n,m = 0,0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+visit = [[0]*5 for _ in range(5)]
+def OOB(x,y):
+    return x < 0 or x >= n or y < 0 or y >= m
 
-def A_turn(ar,ac,br,bc,cnt,board):
-    if board[ar][ac] == 0:
-        return (1,cnt)
-    winner = []
-    loser = []
-    flag = False
-    for dr,dc in dir:
-        nr,nc = ar+dr,ac+dc
-        if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
-            flag = True
-            temp = [row[:] for row in board]
-            temp[ar][ac] = 0
-            iswin,turn = B_turn(br,bc,nr,nc,cnt+1,temp)
-            if iswin:
-                winner.append(turn)
-            else:
-                loser.append(turn)
-    if flag:
-        if winner:
-            return (0,min(winner))
-        else:
-            return (1,max(loser))
-    else:
-        return (1,cnt)
+def play(board,curx,cury,opx,opy):
+    global visit
+    if visit[curx][cury]: return 0
+    canWin = 0
+    for i in range(4):
+        nx, ny = curx + dx[i], cury + dy[i]
+        
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
+        
+        if visit[nx][ny] or board[nx][ny] == 0:
+            continue
 
+        visit[curx][cury] = True
+        opResult = play(board,opx,opy,nx,ny)+1
+        visit[curx][cury] = False
 
-def B_turn(br,bc,ar,ac,cnt,board):
-    if board[br][bc] == 0:
-        return (1,cnt)
-    winner = []
-    loser = []
-    flag = False
-    for dr,dc in dir:
-        nr,nc = br+dr,bc+dc
-        if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
-            flag = True
-            temp = [row[:] for row in board]
-            temp[br][bc] = 0
-            iswin,turn = A_turn(ar,ac,nr,nc,cnt+1,temp)
-            if iswin:
-                winner.append(turn)
-            else:
-                loser.append(turn)
-    if flag:
-        if winner:
-            return (0,min(winner))
-        else:
-            return (1,max(loser))
-    else:
-        return (1,cnt)
-
+        # 현재 저장된 값 패배인데 상대가 졌다고 하면 이기는 경우로 저장
+        if canWin % 2 == 0 and opResult % 2 == 1 : canWin = opResult
+        # 현재 저장된 값 패배인데 상대가 이겼다고 하면 최대한 늦게 지는 횟수 선택
+        elif canWin % 2 == 0 and opResult % 2 == 0 : canWin = max(canWin,opResult)
+        # 현재 저장된 값 승리인데 상대가 졌다고 하면 최대한 빨리 이기는 횟수 선택
+        elif canWin % 2 == 1 and opResult % 2 == 1 : canWin = min(canWin,opResult)
+    return canWin
 
 def solution(board, aloc, bloc):
-    ar,ac,br,bc = aloc[0],aloc[1],bloc[0],bloc[1]
-    answer = A_turn(ar,ac,br,bc,0,board)[1]
-    return answer
+    global n,m
+    n, m = len(board), len(board[0])
+    return play(board,aloc[0],aloc[1],bloc[0],bloc[1])
+
+
+# dir = ((-1,0),(0,1),(1,0),(0,-1))
+
+# def A_turn(ar,ac,br,bc,cnt,board):
+#     if board[ar][ac] == 0:
+#         return (1,cnt)
+#     winner = []
+#     loser = []
+#     flag = False
+#     for dr,dc in dir:
+#         nr,nc = ar+dr,ac+dc
+#         if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
+#             flag = True
+#             temp = [row[:] for row in board]
+#             temp[ar][ac] = 0
+#             iswin,turn = B_turn(br,bc,nr,nc,cnt+1,temp)
+#             if iswin:
+#                 winner.append(turn)
+#             else:
+#                 loser.append(turn)
+#     if flag:
+#         if winner:
+#             return (0,min(winner))
+#         else:
+#             return (1,max(loser))
+#     else:
+#         return (1,cnt)
+
+
+# def B_turn(br,bc,ar,ac,cnt,board):
+#     if board[br][bc] == 0:
+#         return (1,cnt)
+#     winner = []
+#     loser = []
+#     flag = False
+#     for dr,dc in dir:
+#         nr,nc = br+dr,bc+dc
+#         if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
+#             flag = True
+#             temp = [row[:] for row in board]
+#             temp[br][bc] = 0
+#             iswin,turn = A_turn(ar,ac,nr,nc,cnt+1,temp)
+#             if iswin:
+#                 winner.append(turn)
+#             else:
+#                 loser.append(turn)
+#     if flag:
+#         if winner:
+#             return (0,min(winner))
+#         else:
+#             return (1,max(loser))
+#     else:
+#         return (1,cnt)
+
+
+# def solution(board, aloc, bloc):
+#     ar,ac,br,bc = aloc[0],aloc[1],bloc[0],bloc[1]
+#     answer = A_turn(ar,ac,br,bc,0,board)[1]
+#     return answer
--- programmers/LV_3/양과 늑대.py
+++ programmers/LV_3/양과 늑대.py
@@ -1,27 +1,63 @@
-from collections import deque
+def solution(info, edges):
+    visited = [False] * len(info)
+    visited[0] = True
+    global result
+    result = 0
+
+    def dfs(cntSheep, cntWolf, visited):
+        if cntSheep <= cntWolf:
+            return
+
+        global result
+        result = max(result, cntSheep)
+
+        for parent, child in edges:
+            isWolf = info[child]
+
+            if visited[parent] and not visited[child]:
+                visited[child] = True
+                dfs(cntSheep + (isWolf == 0), cntWolf + (isWolf == 1), visited) # 새롭게 알게 된 부분
+                visited[child] = False
+    
+    dfs(1, 0, visited)
+    return result
+
+
+from copy import deepcopy
 
 def solution(info, edges):
-    # 모은 양의 수보다 늑대의 수가 같거나 더 많아지면 바로 모든 양을 잡아먹어 버립
-    result_sheep, result_wolf = 1, 0
     graph = [[] for _ in range(len(info))]
+
     for parent, child in edges:
         graph[parent].append(child)
         
-    queue = deque()
-    queue.append([0, 1, 0])
-    while queue:
-        n, cnt_sheep, cnt_wolf = queue.popleft()
-        for node in graph[n]:
-            if info[node] == 0:
-                result_wolf += 1
-                queue.append([node, result_sheep, result_wolf])
-            elif info[node] == 1:
-                if result_sheep <= result_wolf+1:
-                    continue
-                else:
-                    result_wolf += 1
-                    queue.append([node, result_sheep, result_wolf])
-            
-    return result_sheep
+    global max_sheep
+    max_sheep = 0
+    
+    def dfs(n, cnt_sheep, cnt_wolf, connected):
+        if cnt_sheep <= cnt_wolf:
+            return
+
+        global max_sheep
+        max_sheep = max(max_sheep, cnt_sheep)
+        
+        for i in connected:
+            copy_connected = deepcopy(connected)
+            copy_connected.remove(i)
+            for j in graph[i]:
+                copy_connected.add(j)
+
+            if info[i] == 0:
+                dfs(n, cnt_sheep+1, cnt_wolf, copy_connected)
+            else:
+                dfs(n, cnt_sheep, cnt_wolf+1, copy_connected)
+        
+    connected = set()
+
+    for i in graph[0]:
+        connected.add(i)
+
+    dfs(0, 1, 0, connected)
+    return max_sheep
 
 print(solution([0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1], [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]))
