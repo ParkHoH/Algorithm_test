dd47104ee680fc9b18bff4948bbb5f08d2b28f56
Hohyun Park <ioio6382@gmail.com>
2022-09-20 00:10:29 +0900

    2022-09-20

--- baekjoon/1446_지름길.py
+++ baekjoon/1446_지름길.py
@@ -2,6 +2,7 @@ import heapq
 import sys
 input = sys.stdin.readline
 
+# 다익스트라 풀이
 N, D = map(int, input().split())
 graph = [[(i+1, 1)] for i in range(D+1)]
 
@@ -20,7 +21,7 @@ while heap:
     node, dist = heapq.heappop(heap)
 
     if node == D: break
-    
+
     if distance[node] < dist: continue
 
     for new_node, new_dist in graph[node]:
@@ -29,5 +30,23 @@ while heap:
         if total_dist < distance[new_node]:
             distance[new_node] = total_dist
             heapq.heappush(heap, (new_node, total_dist))
+
+print(distance[-1])
+
+
+# dp 풀이
+N, D = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+distance = [float('inf')] * (D+1)
+distance[0] = 0
+
+for i in range(D+1):
+    if i != 0:
+        distance[i] = min(distance[i], distance[i-1] + 1)
+
+    for a, b, dist in graph:
+        if a == i and a <= D and b <= D:
+            distance[b] = min(distance[b], distance[a] + dist)
 
 print(distance[-1])
