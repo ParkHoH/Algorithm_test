570390ccacbbb6cd97772da9231211a18f2841fe
Hohyun Park <ioio6382@gmail.com>
2022-09-21 18:09:32 +0900

    2022-09-21

---
+++ baekjoon/1865_웜홀.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+
+TC = int(input())
+
+for _ in range(TC):
+    N, M, W = map(int, input().split())
+    graph = defaultdict(int)
+
+    for _ in range(M):
+        a, b, cost = map(int, input().split())
+        
