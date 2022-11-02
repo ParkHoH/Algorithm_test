79e73079e17c4de3aecf911fe3b931cb32523d2e
Hohyun Park <ioio6382@gmail.com>
2022-11-02 21:41:02 +0900

    2022-11-02

--- baekjoon/1103_게임.py
+++ baekjoon/1103_게임.py
@@ -1,28 +1,70 @@
-from collections import deque
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 dx = (-1, 1, 0, 0)
 dy = (0, 0, -1, 1)
 
 N, M = map(int, input().split())
-board = [list(map(int, list(input()))) for _ in range(N)]
+board = [list(input().rstrip()) for _ in range(N)]
 
-def check_possible():
-    visited = [[False] * M for _ in range(N)]
-    visited[0][0] = True
+visited = [[False] * M for _ in range(N)]
+visited[0][0] = True
 
-    queue = deque()
-    queue.append((0, 0, 0)) # x, y, dist
+dp = [[-1] * M for _ in range(N)]
+dp[0][0] = 0
 
-    while queue:
-        x, y, dist = queue.popleft()
+answer = 1
 
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+def dfs(x, y, cnt):
+    num = int(board[x][y])
 
-            if nx < 0 or ny < 0 or nx >= N or ny >= M:
-                continue
+    for i in range(4):
+        nx, ny = x + dx[i]*num, y + dy[i]*num
 
-    return False
+        if nx < 0 or ny < 0 or nx >= N or ny >= M:
+            continue
+
+        if board[nx][ny] == "H" or dp[nx][ny] > cnt+1:
+            continue
+
+        if visited[nx][ny]:
+            print(-1)
+            exit()
+
+        global answer
+        answer = max(answer, cnt+1)
+
+        dp[nx][ny] = cnt+1
+        visited[nx][ny] = True
+        dfs(nx, ny, cnt+1)
+        visited[nx][ny] = False
+
+dfs(0, 0, 1)
+print(answer)
+
+# def check_possible():
+#     visited = [[False] * M for _ in range(N)]
+#     visited[0][0] = True
+
+#     queue = deque()
+#     queue.append((0, 0, 0)) # x, y, dist
+
+#     while queue:
+#         x, y, dist = queue.popleft()
+#         num = board[x][y]
+
+#         for i in range(4):
+#             nx, ny = x + dx[i]*num, y + dy[i]*num
+
+#             if nx < 0 or ny < 0 or nx >= N or ny >= M:
+#                 continue
+            
+#             if visited[nx][ny]:
+#                 return -1
+
+#             visited[nx][ny] = True
+#             visited[]
+
+#     return dist if dist else -1
 
